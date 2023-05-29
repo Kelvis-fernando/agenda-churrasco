c7d2aa9258e6656e960f26b56faa920793d32801
Kelvis Delapena <kelvisdelapena@gmail.com>
2023-05-29 17:03:27 -0300

    feat: created the ehader and the line with user in modal of details

--- src/components/ModalBqqDetails.tsx
+++ src/components/ModalBqqDetails.tsx
@@ -1,4 +1,6 @@
 import {
+  Box,
+  Flex,
   Heading,
   Modal,
   ModalBody,
@@ -7,7 +9,10 @@ import {
   ModalHeader,
   ModalOverlay,
   ModalProps,
+  Radio,
+  Text,
 } from '@chakra-ui/react';
+import { CurrencyCircleDollar, Users } from '@phosphor-icons/react';
 
 const ModalBqqDetails = ({ isOpen, onClose }: ModalProps) => {
   return (
@@ -17,7 +22,70 @@ const ModalBqqDetails = ({ isOpen, onClose }: ModalProps) => {
         <ModalHeader>Churras</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Heading>Detalhes do churras</Heading>
+          <Flex justifyContent="space-between" alignItems="center">
+            <Heading>01/12</Heading>
+            <Flex>
+              <Users size={24} color="#FFD836" />
+              <Text>15</Text>
+            </Flex>
+          </Flex>
+          <Flex justifyContent="space-between" alignItems="center">
+            <Heading>Níver do Gui</Heading>
+            <Flex>
+              <CurrencyCircleDollar size={24} color="#FFD836" />
+              <Text>R$280</Text>
+            </Flex>
+          </Flex>
+          <Box>
+            <Flex
+              justifyContent="space-between"
+              borderBottom="1px solid #FFD836"
+              pb="1"
+            >
+              <Radio
+                colorScheme="red"
+                value="1"
+                display="flex"
+                flexDirection="row"
+                justifyContent="space-around"
+              >
+                Alice
+              </Radio>
+              <Text>R$115</Text>
+            </Flex>
+            <Flex
+              justifyContent="space-between"
+              borderBottom="1px solid #FFD836"
+              pb="1"
+            >
+              <Radio
+                colorScheme="red"
+                value="1"
+                display="flex"
+                flexDirection="row"
+                justifyContent="space-around"
+              >
+                Alice
+              </Radio>
+              <Text>R$115</Text>
+            </Flex>
+            <Flex
+              justifyContent="space-between"
+              borderBottom="1px solid #FFD836"
+              pb="1"
+            >
+              <Radio
+                colorScheme="red"
+                value="1"
+                display="flex"
+                flexDirection="row"
+                justifyContent="space-around"
+              >
+                Alice
+              </Radio>
+              <Text>R$115</Text>
+            </Flex>
+          </Box>
         </ModalBody>
       </ModalContent>
     </Modal>
