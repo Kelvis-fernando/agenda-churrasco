f4d96e46fd7e2fd406cb09c3b4cf4669846bed4f
Kelvis Delapena <kelvisdelapena@gmail.com>
2023-05-31 16:22:14 -0300

    chore: added some changes in login

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,7 +1,69 @@
 import { Box } from '@chakra-ui/layout';
+import { FormControl, FormLabel, Input, Button, Flex } from '@chakra-ui/react';
+import background from '../../assets/background.svg';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return <Box></Box>;
+  return (
+    <Box
+      h="85vh"
+      w="100%"
+      minH="30%"
+      backgroundImage={`url(${background})`}
+      backgroundSize="100% auto"
+    >
+      <FormControl
+        mt={100}
+        isRequired
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        textAlign="left"
+      >
+        <FormLabel fontWeight="bold">E-mail</FormLabel>
+        <Input
+          type="email"
+          placeholder="seunome@exemplo.com"
+          bgColor="white"
+          height="50"
+          w="50%"
+          minW="300"
+          borderRadius="sm"
+        />
+        <FormLabel fontWeight="bold">Senha</FormLabel>
+        <Input
+          type="password"
+          placeholder="senha"
+          bgColor="white"
+          height="50"
+          minW="300"
+          w="50%"
+          borderRadius="sm"
+        />
+        <Button
+          mt="5"
+          bgColor="black"
+          color="white"
+          minW="300"
+          w="20%"
+          fontWeight="bold"
+          _hover={{ bgColor: 'rgba(0, 0, 0, 0.8)' }}
+        >
+          Entrar
+        </Button>
+        <Flex
+          fontWeight="bold"
+          justifyContent="space-between"
+          w="50%"
+          minW={300}
+        >
+          <Link to="/register">Cadastre-se</Link>
+          <Link to="/forgot-password">Esqueceu a senha</Link>
+        </Flex>
+      </FormControl>
+    </Box>
+  );
 };
 
 export default Login;
